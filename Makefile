b8fcc96cd4a7c95c9a23869821cacdc198b898fc
Matthew Stoodley <matt.storyfeet@gmail.com>
2023-06-20 22:09:33 +0100

    wasm converter incoming

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 site:
 	siter gen -o docs
+
+
+wasm:
+	cd zig; zig build-lib main.zig -target wasm32-freestanding -dynamic
+	cp zig/main.wasm static/wasm/main.wasm
--- zig/convert.zig
+++ zig/convert.zig
@@ -6,103 +6,114 @@ const TType = token.TokenType;
 const WType = fmap.WordType;
 const GPAlloc = std.heap.GeneralPurposeAllocator(.{});
 
+var gpa = GPAlloc{};
 
-const Converter = struct{
-    words: fmap.FontMap,
-    letters : fmap.FontMap,
-    s : []const u8,
-    w : std.fs.File.Writer,
-    prev: WType,
 
-    ///Does not take ownership of string
-    pub fn convert(s:[]const u8,w:std.fs.File.Writer) !void{
-	var self = @This() {
-	    .words = try fmap.fontMap(),
-	    .letters = try fmap.letterMap(),
-	    .s = s,
-	    .w = w,
-	    .prev = WType.Norm,
-	};
-	//defer self.free();
-	var tk = token.Tokenizer.init(self.s);
-	while (try tk.nextToken()) |t| {
-	    try self.writeSymbol(t);
-	}
+fn Converter(comptime tp:type)type{ 
+    return struct{
+	words: fmap.FontMap,
+	letters : fmap.FontMap,
+	s : []const u8,
+	w : tp,
+	prev: WType,
 
-	
-    }
-
-    pub fn free(self:@This())void {
-	self.words.clearAndFree();
-	self.letters.clearAndFree();
-    }
-
-    pub fn writeSymbol(self:*@This(),t: token.Token)!void{
-	const w = self.w;
-	const s = self.s;
-	switch(t.kind) {
-	    TType.QUOTE => try w.print(" {u}",.{0xe0e6}),
-	    TType.STOP => try w.print("{u} ",.{0xe0e7}),
-	    TType.COMMA => try w.print("{u} ",.{0xe0e8}),
-	    TType.DASH => {},
-	    TType.NEWLINE => try w.print("\n",.{}),
-	    TType.COMMENT => try w.print("{s}",.{s[t.start..t.end]}),
-	    TType.WORD => {
-		try self.printWord(t);
-	    },
-	}
-    }
-
-    pub fn printWord(self:*@This(),t:token.Token) !void{
-
-	const wd = self.s[t.start..t.end];
-	const w = self.w;
-	if (self.words.get(wd)) |fp| {
-	    var space = false;
-	    if (fp.wType == WType.Par and self.prev == WType.Norm ){
-		space = true;
+	///Does not take ownership of string
+	pub fn convert(s:[]const u8,w:tp) !void{
+	    var self = @This() {
+		.words = try fmap.fontMap(),
+		.letters = try fmap.letterMap(),
+		.s = s,
+		.w = w,
+		.prev = WType.Norm,
+	    };
+	    //defer self.free();
+	    var tk = token.Tokenizer.init(self.s);
+	    while (try tk.nextToken()) |t| {
+		try self.writeSymbol(t);
 	    }
-	    if (space) {
-		try w.print(" {u}",.{fp.code});
-	    }else {
-		try w.print("{u}",.{fp.code});
-	    }
-	    self.prev = fp.wType;
-	} else {
-	    try w.print(" ",.{});
-	    var it = std.unicode.Utf8Iterator{.bytes=wd,.i=0};
-	    while (it.nextCodepointSlice()) |c|{
-		if (self.letters.get(c)) |fp|{
-		    try w.print("{u}",.{fp.code});
-		}else {
-		    try w.print("{s}",.{c});
-		}
-	    }
-	    try w.print(" ",.{});
-	    self.prev = WType.Norm;
 	    
 	}
 
-    }
+	pub fn free(self:@This())void {
+	    self.words.clearAndFree();
+	    self.letters.clearAndFree();
+	}
+
+	pub fn writeSymbol(self:*@This(),t: token.Token)!void{
+	    const w = self.w;
+	    const s = self.s;
+	    switch(t.kind) {
+		TType.QUOTE => try w.print(" {u}",.{0xe0e6}),
+		TType.STOP => try w.print("{u} ",.{0xe0e7}),
+		TType.COMMA => try w.print("{u} ",.{0xe0e8}),
+		TType.DASH => {},
+		TType.NEWLINE => try w.print("\n",.{}),
+		TType.COMMENT => try w.print("{s}",.{s[t.start..t.end]}),
+		TType.WORD => {
+		    try self.printWord(t);
+		},
+	    }
+	}
+
+	pub fn printWord(self:*@This(),t:token.Token) !void{
+
+	    const wd = self.s[t.start..t.end];
+	    const w = self.w;
+	    if (self.words.get(wd)) |fp| {
+		var space = false;
+		if (fp.wType == WType.Par and self.prev == WType.Norm ){
+		    space = true;
+		}
+		if (space) {
+		    try w.print(" {u}",.{fp.code});
+		}else {
+		    try w.print("{u}",.{fp.code});
+		}
+		self.prev = fp.wType;
+	    } else {
+		try w.print(" ",.{});
+		var it = std.unicode.Utf8Iterator{.bytes=wd,.i=0};
+		while (it.nextCodepointSlice()) |c|{
+		    if (self.letters.get(c)) |fp|{
+			try w.print("{u}",.{fp.code});
+		    }else {
+			try w.print("{s}",.{c});
+		    }
+		}
+		try w.print(" ",.{});
+		self.prev = WType.Norm;
+		
+	    }
+
+	}
+
+    };
+
+}
 
 
 
-};
+export fn convert(s:[*]const u8,len:u32)void {
+    var alloc = gpa.allocator();
+    var ss = s[0..len];
 
-
+    var res = std.ArrayList(u8).init(alloc);
+    Converter(std.ArrayList(u8).Writer).convert(ss,res.writer()) catch return ;
+    //return &res.toOwnedSlice();
+    
+}
 
 
 
 pub fn main() !void {
 
-    var gpa = GPAlloc{};
     var alloc = gpa.allocator();
 
     var stdin = std.io.getStdIn();
     const in_str = try stdin.readToEndAlloc(alloc,100_000);
     defer alloc.free(in_str);
 
-    try Converter.convert(in_str,std.io.getStdOut().writer());
+    try Converter(std.fs.File.Writer).convert(in_str,std.io.getStdOut().writer());
     
 
 }
