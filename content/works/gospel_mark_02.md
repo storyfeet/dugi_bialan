bcede888d26bde7d5aab4bd09b5a8f6a3d58c5a5
Matthew Stoodley <matt.storyfeet@gmail.com>
2023-12-01 17:02:14 +0000

    now handles html tags in convert, continued gospel of mark2

--- content/works/gospel_mark_02.md
+++ content/works/gospel_mark_02.md
@@ -1,12 +1,30 @@
 {{export title = "Gospel of Mark:  Chapter 2"}}
 
 {{@md}}{{@exec_stdin "zig/convert" @ }}
+
+won : zan va-gein
+#---------
+
 devin leli esuno la lon don bi i zan Zesuwa wei ensa ei man Giabenewamm la muli zan wei gudei ei i is-mon wei guma.
 
 zan wei guma ba zelula la va-li luga ei man va-laban, ba lon wega viala. is-mon wei dugi zon ei enon dugi da is-mudei.
 
 leli zan wei guma da is-mon gin zan va-gein. dan i is-leli va-wei gein da zan Zezuwa dan zan zan la is-leli wei luli ei zugi mengi wei gan-oiga ei is-va-gein lon zugi liabei gin lena.
 
-lon wei lugin ei esebin gein is-leli la, zan Zesuwa da zan va-gein go "o zi mi, egi vin li wei bena wega."
+lon wei lugin ei esebin gein is-leli la, zan Zesuwa da zan va-gein go <span class="red">"o zi mi, egi vin li wei bena wega."</span>
+
+mudei zan bi bena zon bi liawa wei moini lon man dis, wei esebin lon is-mudei go "dan vemi i zan dis wei dugi egin dis, vemi li gein bena wega ei egi va-deso enma veli"
+
+lon don egesa dis, zan Zesuwa wei zon ei esebin bi guli-belin is-mudei, go <span class="red">"vin dan vemi wei esebin ei dis. vemi li gein gein mudei? da zan dis go "egi vin li wei bena wega" iani go "o gan zewi ei vin ei zugi liabei vin o giawin oiga". deso la, da wei bena lugin da vin ei i zi mon wei gein bena wega ei egi""</span> da zan va-gain go <span class="red">"o gan-zewi ei vin ei zugi liabei vin o giawin da mengi vin"</span>
+
+is-va-gein wei gan-zewi ei begi ei zugi labei begi wei wega begi lon enbin lugin muli zan.  muli zan li belin wiawa wei bena wiawa da enma veli go, "mi lon va-don wei lugin ei mugi egin dis"
+
+mas van : dugi guma da zan Levi
+#--------
+
+
+
+
+
 
 
--- zig/token.zig
+++ zig/token.zig
@@ -1,143 +1,146 @@
 const std = @import("std");
 
-pub const TokenType = enum{
-	WORD,
-	COMMA,
-	STOP,
-	QUOTE,
-	COMMENT,
-	DASH,
-	NEWLINE,
+pub const TokenType = enum {
+    WORD,
+    COMMA,
+    STOP,
+    QUOTE,
+    COMMENT,
+    DASH,
+    NEWLINE,
 };
 
-pub const specialChars = &[10]u21{'\\','\n','\r',' ','\t',',','.','#','-','"'};
+pub const specialChars = &[10]u21{ '\\', '\n', '\r', ' ', '\t', ',', '.', '#', '-', '"' };
 
-pub fn special(c:u21)bool{
-	for (specialChars) |s| {
-		if (c == s) {
-			return true;
-		}
-	}
-	return false;
+pub fn special(c: u21) bool {
+    for (specialChars) |s| {
+        if (c == s) {
+            return true;
+        }
+    }
+    return false;
 }
 
-pub const Token = struct{
-	kind : TokenType,
-	start : usize,
-	end : usize,
-	line : usize,
+pub const Token = struct {
+    kind: TokenType,
+    start: usize,
+    end: usize,
+    line: usize,
 };
 
+pub const Tokenizer = struct {
+    it: std.unicode.Utf8Iterator,
+    start: usize,
+    line: usize,
 
-pub const Tokenizer = struct{
-	it : std.unicode.Utf8Iterator,
-	start : usize,
-	line : usize,
+    pub fn init(s: []const u8) @This() {
+        return @This(){
+            .it = std.unicode.Utf8Iterator{ .bytes = s, .i = 0 },
+            .start = 0,
+            .line = 1,
+        };
+    }
 
-	pub fn init(s: []const u8) @This() {
-		return @This() {
-			.it = std.unicode.Utf8Iterator{.bytes=s,.i=0},
-			.start = 0,
-			.line = 1,
-		};
-	}
+    pub fn nextToken(self: *@This()) !?Token {
+        self.skipWhiteSpace();
+        self.start = self.it.i;
+        var c = self.it.nextCodepoint() orelse return null;
+        switch (c) {
+            ',' => return self.makeToken(TokenType.COMMA),
+            '.' => return self.makeToken(TokenType.STOP),
+            '-' => return self.makeToken(TokenType.DASH),
+            '"' => return self.makeToken(TokenType.QUOTE),
+            '\n' => {
+                var tk = self.makeToken(TokenType.NEWLINE);
+                self.line += 1;
+                return tk;
+            },
+            '\\' => return self.escape(),
+            '<' => return self.tag(),
+            '#' => return try self.comment(),
+            else => return self.word(),
+        }
+    }
 
-	pub fn nextToken(self:*@This()) !?Token {
-		self.skipWhiteSpace();
-		self.start = self.it.i;
-		var c = self.it.nextCodepoint() orelse return null;
-		switch (c) {
-			',' => return self.makeToken(TokenType.COMMA),
-			'.' => return self.makeToken(TokenType.STOP),
-			'-' => return self.makeToken(TokenType.DASH),
-			'"' => return self.makeToken(TokenType.QUOTE),
-			'\n' => {
-				var tk = self.makeToken(TokenType.NEWLINE);
-				self.line += 1;
-				return tk;
-			},
-			'\\' => return self.escape(),
-			'#' => return try self.comment(),
-			else => return self.word(),
-		}
-	}
+    pub fn escape(self: *@This()) ?Token {
+        self.start = self.it.i;
+        _ = self.it.nextCodepoint();
+        return self.makeToken(TokenType.COMMENT);
+    }
+    pub fn tag(self: *@This()) !?Token {
+        while (true) {
+            var c = self.it.nextCodepoint() orelse return error.UnexpectedEOFInHtmlTag;
+            if (c == '>') return self.makeToken(TokenType.COMMENT);
+        }
+    }
+    pub fn comment(self: *@This()) !Token {
+        self.start = self.it.i;
+        while (true) {
+            var pk = self.it.peek(1);
+            if (pk.len == 0) {
+                return self.makeToken(TokenType.COMMENT);
+            }
+            var c = try std.unicode.utf8Decode(pk);
+            switch (c) {
+                '\n' => {
+                    return self.makeToken(TokenType.COMMENT);
+                },
+                '#' => {
+                    var tk = self.makeToken(TokenType.COMMENT);
+                    _ = self.it.nextCodepoint();
+                    return tk;
+                },
+                else => _ = self.it.nextCodepoint(),
+            }
+        }
+    }
 
-	pub fn escape(self:*@This()) ?Token{
-		self.start = self.it.i;	
-		_ = self.it.nextCodepoint();
-		return self.makeToken(TokenType.COMMENT);
-	}
-	pub fn comment(self:*@This()) !Token{
-		self.start = self.it.i;
-		while(true){
-			var pk = self.it.peek(1);
-			if (pk.len == 0) {
-				return self.makeToken(TokenType.COMMENT);
-			}
-			var c = try std.unicode.utf8Decode(pk);
-			switch(c) {
-				'\n' => {
-					return self.makeToken(TokenType.COMMENT);
-				},
-				'#' => {
-					var tk = self.makeToken(TokenType.COMMENT);	
-					_ = self.it.nextCodepoint();
-					return tk;
-				},
-				else => _ = self.it.nextCodepoint(),
-			}
-		}
-	}
+    pub fn word(self: *@This()) Token {
+        while (true) {
+            const pk = self.it.peek(1);
+            if (pk.len == 0) return self.makeToken(TokenType.WORD);
+            const cp = std.unicode.utf8Decode(pk) catch return self.makeToken(TokenType.WORD);
+            if (special(cp)) {
+                return self.makeToken(TokenType.WORD);
+            }
+            _ = self.it.nextCodepoint();
+        }
+    }
 
-	pub fn word(self: *@This()) Token {
-		while(true){
-			const pk = self.it.peek(1);
-			if (pk.len == 0) return self.makeToken(TokenType.WORD);
-			const cp = std.unicode.utf8Decode(pk) 
-				catch return self.makeToken(TokenType.WORD);
-			if (special(cp)) {
-				return self.makeToken(TokenType.WORD);
-			}
-			_ = self.it.nextCodepoint();
-				
-		}
-	}
+    pub fn makeToken(self: *@This(), tt: TokenType) Token {
+        return Token{
+            .kind = tt,
+            .start = self.start,
+            .end = self.it.i,
+            .line = self.line,
+        };
+    }
 
-	pub fn makeToken(self:*@This(),tt:TokenType) Token{
-		return Token {
-			.kind = tt,
-			.start = self.start,
-			.end = self.it.i,
-			.line = self.line,
-		};
-	}
-
-	pub fn skipWhiteSpace(self:*@This()) void {
-		while(true){
-			const c = self.it.peek(1);
-			if (c.len == 0) return;
-			const cp = std.unicode.utf8Decode(c) catch return;
-			switch (cp) {
-				' ', '\t', '\r' => _ = self.it.nextCodepoint(),
-				else => return,
-			}
-		}
-	}
+    pub fn skipWhiteSpace(self: *@This()) void {
+        while (true) {
+            const c = self.it.peek(1);
+            if (c.len == 0) return;
+            const cp = std.unicode.utf8Decode(c) catch return;
+            switch (cp) {
+                ' ', '\t', '\r' => _ = self.it.nextCodepoint(),
+                else => return,
+            }
+        }
+    }
 };
 
-
-const TErr = error {
-	IsNull,
+const TErr = error{
+    IsNull,
 };
 
-test "can compile anything"{
-	const ex = std.testing.expect;
-	const s = ",Hello, people";
-	var tk = Tokenizer.init(s);
-	const cm = try tk.nextToken() orelse return error.IsNull;
-		
-	try std.testing.expect(cm.kind == TokenType.COMMA);
-	const hel = try tk.nextToken() orelse return error.IsNull;
-	try ex(hel.kind == TokenType.WORD);
-	try ex(std.mem.eql(u8,s[hel.start..hel.end] ,"Hello"));
+test "can compile anything" {
+    const ex = std.testing.expect;
+    const s = ",Hello, people";
+    var tk = Tokenizer.init(s);
+    const cm = try tk.nextToken() orelse return error.IsNull;
+
+    try std.testing.expect(cm.kind == TokenType.COMMA);
+    const hel = try tk.nextToken() orelse return error.IsNull;
+    try ex(hel.kind == TokenType.WORD);
+    try ex(std.mem.eql(u8, s[hel.start..hel.end], "Hello"));
 }
