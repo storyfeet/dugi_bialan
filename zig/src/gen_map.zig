f6d1c2b0dfe08c664bda21f6c46fe345997a8991
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-06-21 18:54:15 +0100

    now reads util words to create gen_map

--- zig/src/gen_map.zig
+++ zig/src/gen_map.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 var AL = std.heap.GeneralPurposeAllocator(.{}){};
 var al = AL.allocator();
 
+const crd = @import("card_format");
+
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     var f = std.fs.cwd().openFile("../util/word_list.crd", .{}) catch |e| {
@@ -16,7 +18,30 @@ pub fn main() !void {
     var fout = try std.fs.cwd().createFile("gen/words.zig", .{ .truncate = true });
     defer fout.close();
 
-    try fout.writeAll(s);
+    var p = try crd.Parser.init(s, al);
+    defer p.deinit();
+    var name: []const u8 = try al.dupe(u8, "NOTHING");
+
+    while (true) {
+        var cdata = try p.next() orelse break;
+
+        switch (cdata) {
+            .NAME => |cname| {
+                al.free(name);
+                name = cname;
+            },
+            .KEYVAL => |*kv| {
+                if (std.mem.eql(u8, kv.k, "sign")) {
+                    try fout.writer().print("{s}  -  {s}\n", .{ name, kv.v.Str });
+                }
+                kv.deinit(al);
+            },
+            else => {
+                continue;
+            },
+        }
+    }
+
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
 
 }
