102db3b47125bf057a8a62a1da0fbb8334810f8e
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-09-06 21:56:53 +0100

    build out basic question getter logic for quiz

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,25 +1,165 @@
-class WordList {
-  constructor(wordList) {
-    this.startList = wordList;
+
+function nextReady(now,score,error){
+	Date(now.getTime()+ 1000 * Math.pow(2,score));
+}
+
+
+class QuizManager {
+  constructor(wordList,display) {
+	//StartList should not change after construction
+    this.wordList = wordList;
+	//active list shold change regularly
     this.activeList = [];
+	this.nextAdd = 0;
+
   }
 
+	nextQuestion(){
+		// Create a set of optional words, 
+		let ans = this.nextWord();
+		if (! ans){
+			return null;
+		}
+
+		return {
+			active:ans,	
+			options: this.chooseOptions(ans),
+		};
+		
+	}
+
+
+	nextToAdd(){
+		outer: for (let i = 0 ;i < this.wordList.length; i++){
+			for(let a of this.activeList){
+				if (a.index == i){
+					continue outer;
+				}
+			}
+			return i;
+		}
+		return null;
+	}
+
+	
+	addWord(toAdd = null){
+		let nextAdd = toAdd ?? this.nextToAdd();
+		if (this.nextAdd == null){
+			return null;
+		}
+		let res = {
+			index: nextAdd,
+			name: this.wordList[nextAdd].name,
+			nextReady : Date.now(),
+			score: 0,
+			error: false,
+			buddies: [],
+		};
+		this.activeList.push(res);
+		return res; 
+	}
+
+	/**
+	* @return Active Element
+	*/
   nextWord() {
     let ready = this.activeList
-      .filter((item) => item.nextReady < time.Now())
+      .filter((item) => item.nextReady < Date.Now())
       .sort((a, b) => a.nextReady - b.nextReady);
 
     if (ready.length == 0) {
-      /// Choose a new word
-      return;
+		return this.addWord(); 
     }
 
     let n = Math.floor(
-      Math.min(Math.rand(), Math.rand(), Math.rand()) * ready.length,
+      Math.min(Math.random(), Math.random(), Math.random()) * ready.length,
     );
+	return ready[n];
   }
+
+
+	findBuddy(name){
+		for(let a of this.activeList){
+			if (a.name === name){
+				return a;
+			}
+		}
+		for (let i = 0; i < this.wordList.length; i++){
+			if (this.wordList[i].name === name){
+				return this.addWord(i);
+			}
+		}
+		return null;
+	}
+
+	randomOption(){
+		let i = Math.floor(Math.random() * (this.activeList.length + 3));
+		if (i < this.activeList.length){
+			return this.activeList[i];
+		}
+		return this.addWord();
+	}
+
+	chooseOptions(activity){
+		let res = activity.buddies.map((bud)=>this.findBuddy(bud));
+		while (res.length < 4){
+			let op = this.randomOption();
+			if (op) {
+				res.push(op);
+			}
+		}
+		return res;
+	}
+
+
+	success(index){
+	}
+	
+	fail(index,mistakeSelected){
+	}
 }
 
+
+class WebDisplay{
+		
+	renderQuestion(word,score){
+		return 
+	}
+}
+
+
+class CliDisplay{
+	getMode(score){
+		return 1;
+	}
+	renderQuestion(word,mode){
+		return word.q;
+	}
+
+	renderOption(word,mode){
+		return word.a;
+	}
+	
+	renderAnswer(word,mode){
+	}
+}
+
+
+
+
+
+
 if (require?.main === module) {
-  console.log("Is running main");
+	//MAIN
+	let quizMan = new QuizManager([
+		{name:"Hello",a:"o buna"},
+		{name:"Goodbye",a:"o yawin buna"},
+		{name:"Fish",a:"gela zo"},
+		{name:"This",a:"this"},
+		{name:"That",a:"that"},
+		{name:"Open",a:"ubin"},
+	]);
+
+	console.log(quizMan.nextQuestion());
+
 }
