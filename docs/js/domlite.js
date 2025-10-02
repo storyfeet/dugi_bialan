9458c11182a6852ca95dabcefc27fd8d31ba3cf4
Matthew Stoodley <matt.storyfeet@gmail.com>
2025-10-02 20:54:10 +0100

    generate

--- docs/js/domlite.js
+++ docs/js/domlite.js
@@ -2,7 +2,11 @@
 export function tag(name,props = {},children = []){
 	let res = document.createElement(name);
 	for (let [key,val] of Object.entries(props)){
+		res.setAttribute(key, val);
 		res[key] = val;
+	}
+	if (props.className){
+		res.classList.add(props.className);
 	}
 	if (typeof children[Symbol.iterator] === 'function'){
 		res.append(...children);	
@@ -25,8 +29,28 @@ export function br(){
 	return tag('br');
 }
 
+export function span(txt,props = {},children = []){
+	return tag('span',props,[tx(txt),...children]);
+}
+
+export function table(props = {},children = []){
+	return tag('table',props,children);
+}
+
+export function tr(props = {},children = []){
+	return tag('tr',props,children);
+}
+
+export function td(props = {},children = []){
+	return tag('td',props,children);
+}
+
 export default {
-	tag:tag,
-	tx:tx,
 	br,
+	span:span,
+	table:table,
+	tag:tag,
+	td:td,
+	tr:tr,
+	tx:tx,
 };
